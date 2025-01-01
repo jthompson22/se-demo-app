7bf836ff38a85ee6b3244835889ea0619763cfe5
Jared Thompson <jared.thompson@vercel.com>
2025-01-01 12:30:06 -0700

    Quick sneak

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId: 'aoxxjzpu', // from your sanity.config.ts
-  dataset: 'production',
-  apiVersion: '2024-03-01',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, // from your sanity.config.ts
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
   useCdn: true,
 });
